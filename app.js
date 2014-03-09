5a0e64641c35c4b8e42eb929bd85f99f6c478acd
Tjiu Suryanto <tjiu.suryanto@gmail.com>
2014-03-09 20:01:26 +0700

    Updated port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 2000);
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 2000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
